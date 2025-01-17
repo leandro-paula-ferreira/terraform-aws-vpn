799cb3f2a25a193f6912d3052d37194523ef052a
Leandro de Souza Paula Ferreira <leandrospferreira@gmail.com>
2025-01-17 18:04:20 -0300

    ajuste do cidr_blocks

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ resource "aws_security_group" "vpn_access" {
     from_port        = 0
     to_port          = 0
     protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
+    cidr_blocks = ["10.0.0.0/16"]
     ipv6_cidr_blocks = ["::/0"]
   }
 }
